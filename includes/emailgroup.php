d69f3180906cebed6deaefdc51f6f493493d724d
Russ Hewson <russ.hewson+github@gmail.com>
2016-02-25 14:19:31 +0000

    Updated email group text

--- includes/emailgroup.php
+++ includes/emailgroup.php
@@ -1,5 +1,5 @@
 <?php
-echo '<h3>Email investigators</h3>';
+echo '<h3>Investigator email lists</h3>';
 $form = new HTMLForm( 'process.php', 'post' );
 $fields = $trial->getFormFields( $page );
 $form->processFields( $fields );
@@ -8,5 +8,6 @@ if ( isset( $_SESSION['inputErr'] ) ) { // If any errors then add them to the fo
     unset( $_SESSION['inputErr'] );
 }
 echo $form->writeHTML(false, true);
+echo "<h4>Emails:</h4>";
 echo "<div class='emailAddresses well'></div>";
 ?>
