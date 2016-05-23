2fc10ad99b8e454f3f871d864ba8c54f76160deb
Russ Hewson <russ.hewson+github@gmail.com>
2016-05-24 00:19:45 +0100

    Changes default user and site table length to 25

--- js/ecrf.js
+++ js/ecrf.js
@@ -267,6 +267,7 @@ jQuery( function() {
     
     $( '.dataTable' ).dataTable( {
         "sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+	"iDisplayLength": 25,
         "sPaginationType": "bootstrap",
         "oLanguage": {
 			"sLengthMenu": "_MENU_ records per page"
