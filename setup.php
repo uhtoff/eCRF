3f554336669dc25d2f2166a9a6ceee835383b751
Russ Hewson <russ.hewson+github@gmail.com>
2016-06-28 14:39:55 +0100

    Revert "UPdated basic setup info"
    This reverts commit 45e97c9eb1624ab3148ea8b03130de1960a78eef.

--- setup.php
+++ setup.php
@@ -10,11 +10,11 @@ require( $path . '/libs/serverconfig.php');
 addIncludePath('/classes');
 addIncludePath('/addons');
 
-$trial = 'OPTIMISE II';
+$trial = 'PRISM';
 Config::set('userdb', $db);
 Config::set('database', $db);
 Config::set('trial', $trial);
-Config::set('idName', 'OPTIMISE II ID');
+Config::set('idName', 'PRISM ID');
 
 if ( !DB::setDB($db) ) {
     exit( 'Unable to set database' );
@@ -22,5 +22,5 @@ if ( !DB::setDB($db) ) {
 
 require( 'ecrflib.php' );
 require( 'mainlib.php' );
-session_name('OPTIMISEII');
+session_name('PRISM');
 ?>
