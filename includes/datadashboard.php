3e1ea93be9e2ca0e8c37ba8238dac61578f0030c
Russ Hewson <russ.hewson+github@gmail.com>
2016-08-26 16:54:40 +0100

    Added length of CPAP checking to the Data dashboard

--- includes/datadashboard.php
+++ includes/datadashboard.php
@@ -1,6 +1,14 @@
 <?php
 echo "<div class='container well'>";
-$sql = "SELECT violation.nocpap, violation.violationdesc, core.trialid, studygroup, cpap.cpap FROM core LEFT JOIN link ON core.id = link.core_id LEFT JOIN cpap ON link.cpap_id = cpap.id LEFT JOIN violationlink ON link.id = violationlink.link_id LEFT JOIN violation ON violationlink.violation_id = violation.id WHERE studygroup = 1 AND cpap.cpap = 0 AND link.discontinue_id IS NULL";
+$sql = "SELECT SUM(violation.nocpap) AS nocpap, violation.violationdesc, core.trialid, studygroup, cpap.cpap FROM core 
+LEFT JOIN link ON core.id = link.core_id 
+LEFT JOIN cpap ON link.cpap_id = cpap.id 
+LEFT JOIN violationlink ON link.id = violationlink.link_id 
+LEFT JOIN violation ON violationlink.violation_id = violation.id 
+WHERE studygroup = 1 
+AND cpap.cpap = 0 
+AND link.discontinue_id IS NULL
+GROUP BY core.trialid";
 $result = DB::query($sql);
 $nodeviation = array();
 $deviation = array();
@@ -35,12 +43,20 @@ if (!empty($nodeviation) || !empty($deviation)) {
     echo "<p>No patients assigned to the study group did not receive CPAP</p>";
 }
 
-$sql = "SELECT violation.nocpap, violation.violationdesc, core.trialid, studygroup, cpap.cpap FROM core LEFT JOIN link ON core.id = link.core_id LEFT JOIN cpap ON link.cpap_id = cpap.id LEFT JOIN violationlink ON link.id = violationlink.link_id LEFT JOIN violation ON violationlink.violation_id = violation.id WHERE studygroup = 0 AND cpap.cpap = 1 AND link.discontinue_id IS NULL";
+$sql = "SELECT SUM(violation.nocpap) as nocpap, violation.violationdesc, core.trialid, studygroup, cpap.cpap FROM core 
+LEFT JOIN link ON core.id = link.core_id 
+LEFT JOIN cpap ON link.cpap_id = cpap.id 
+LEFT JOIN violationlink ON link.id = violationlink.link_id 
+LEFT JOIN violation ON violationlink.violation_id = violation.id 
+WHERE studygroup = 0 
+AND cpap.cpap = 1 
+AND link.discontinue_id IS NULL
+GROUP BY core.trialid";
 $result = DB::query($sql);
 $nodeviation = array();
 $deviation = array();
 foreach ($result->rows as $row) {
-    if (is_null($row->nocpap)) {
+    if (is_null($row->nocpap) || !$row->nocpap) {
         $nodeviation[] = $row->trialid;
     } else {
         $deviation[] = $row->trialid;
@@ -68,6 +84,50 @@ if (!empty($nodeviation) || !empty($deviation)) {
     echo "</ul>";
 } else {
     echo "<p>No patients assigned to the control group have received CPAP</p>";
+}
+
+$sql = "SELECT SUM(violation.stopcpap) as stopcpap, violation.violationdesc, core.trialid, studygroup, cpap.cpap, cpap.cpapduration FROM core 
+LEFT JOIN link ON core.id = link.core_id 
+LEFT JOIN cpap ON link.cpap_id = cpap.id 
+LEFT JOIN violationlink ON link.id = violationlink.link_id 
+LEFT JOIN violation ON violationlink.violation_id = violation.id 
+WHERE studygroup = 1 
+AND cpap.cpap = 1 
+AND link.discontinue_id IS NULL
+AND cpapduration < 240
+GROUP BY core.trialid";
+$result = DB::query($sql);
+$nodeviation = array();
+$deviation = array();
+foreach ($result->rows as $row) {
+    if (is_null($row->stopcpap) || !$row->stopcpap) {
+        $nodeviation[] = $row->trialid;
+    } else {
+        $deviation[] = $row->trialid;
+    }
+}
+if (!empty($nodeviation) || !empty($deviation)) {
+    echo "<p>All the following trial IDs were assigned to the trial group and received less than 240 minutes of CPAP</p>";
+    echo "<ul>";
+    if (!empty($nodeviation)) {
+        echo "<li>The following Trial IDs have no appropriate Protocol Deviation form entered</li>";
+        echo "<ul>";
+        foreach ($nodeviation as $id) {
+            echo "<li>{$id}</li>";
+        }
+        echo "</ul>";
+    }
+    if (!empty($deviation)) {
+        echo "<li>The following Trial IDs have an appropriate Protocol Deviation form entered</li>";
+        echo "<ul>";
+        foreach ($deviation as $id) {
+            echo "<li>{$id}</li>";
+        }
+        echo "</ul>";
+    }
+    echo "</ul>";
+} else {
+    echo "<p>All patients assigned to the study group and receiving CPAP received at least 240 minutes of CPAP</p>";
 }
 
 $records = $trial->getAllRecords();
